f85b7f342cf2300c78e71e211b6dbdcab2cfa98e
Thinh Tran <thinhrom11@gmail.com>
2025-01-31 17:34:28 +0700

    style: Add prettier and eslint to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,22 @@
 // @ts-check
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
-import vue from '@astrojs/vue';
+import sitemap from "@astrojs/sitemap";
+import vue from "@astrojs/vue";
 
-import tailwindcss from '@tailwindcss/vite';
-import {defineConfig} from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+import icon from "astro-icon";
+
+import robotsTxt from "astro-robots-txt";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    // Enable Vue to support Vue components.
-    integrations: [vue(), mdx(), sitemap()],
+  // Enable Vue to support Vue components.
+  integrations: [vue(), mdx(), sitemap(), icon(), robotsTxt()],
 
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,5 +1,5 @@
-<script setup lang="ts">
-import { ref } from 'vue';
+<script lang="ts" setup>
+import { ref } from "vue";
 
 const count = ref(0);
 const add = () => count.value++;
@@ -7,26 +7,26 @@ const subtract = () => count.value--;
 </script>
 
 <template>
-	<div class="counter">
-		<button @click="subtract">-</button>
-		<pre>{{ count }}</pre>
-		<button @click="add">+</button>
-	</div>
-	<div class="counter-message">
-		<slot />
-	</div>
+  <div class="counter">
+    <button @click="subtract">-</button>
+    <pre>{{ count }}</pre>
+    <button @click="add">+</button>
+  </div>
+  <div class="counter-message">
+    <slot />
+  </div>
 </template>
 
 <style>
 .counter {
-	display: grid;
-	font-size: 2em;
-	grid-template-columns: repeat(3, minmax(0, 1fr));
-	margin-top: 2em;
-	place-items: center;
+  display: grid;
+  font-size: 2em;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  margin-top: 2em;
+  place-items: center;
 }
 
 .counter-message {
-	text-align: center;
+  text-align: center;
 }
 </style>
